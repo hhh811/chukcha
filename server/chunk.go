a42c8b044da53dfb5b368353ba170c09255863da
root <root@localhost.localdomain>
2022-04-10 21:29:22 +0800

    rm not used file

--- server/chunk.go
+++
@@ -1,8 +0,0 @@
-package server
-
-// Chunk is a piece of data that contains the messages that were written to it.
-// It can be incomplete which means that is currently being written to.
-type Chunk struct {
-	Name     string `json:"name"`
-	Complete bool   `json:"complete"`
-}
